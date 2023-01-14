7a290088d66e16d8fba6d0edfebb76ed348f6733
Walid KROUCHI <117538373+walid0912@users.noreply.github.com>
2023-01-14 12:07:39 +0100

    pushing files

---
+++ ChangeColorOnMouse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ChangeColorOnMouse : MonoBehaviour
+{
+    private Material mat;
+
+    [SerializeField] private Material
+        newMat;
+        
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material = newMat;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = mat;
+    }
+}
---
+++ CreateOnMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateOnMouse : MonoBehaviour
+{
+
+		[SerializeField]
+		private Transform
+				originalObject;
+		private Transform copyObject;
+
+		void Update ()
+		{
+				if (Input.GetButtonDown ("Fire2")) {
+						copyObject = (Transform)Instantiate (originalObject, transform.position, transform.rotation);
+						copyObject.Translate (0, 0, 6);
+				}
+		}
+}
